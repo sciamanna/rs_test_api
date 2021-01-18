43a407f862689b1cf9a22fa621c32c34f81d7604
sciamanna <rebeca.sciamanna@me.com>
2021-01-18 14:46:25 +0000

    Update get to show task for a particular user

--- src/main/java/com/sciamanna/Handler.java
+++ src/main/java/com/sciamanna/Handler.java
@@ -1,37 +1,60 @@
 package com.sciamanna;
+//package com.amazonaws.services.lambda.runtime.events;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sciamanna.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
+//public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 	private static final Logger LOG = LogManager.getLogger(Handler.class);
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
+	//public ApiGatewayResponse handleRequest(ApiGatewayResponse, Context context) {
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+		LOG.info("received the request");
 		//Response responseBody = new Response("Hello Cute! Your function executed successfully!", input);
 
-		Task t1 = new Task ("t001", "Iron", false);
-		Task t2 = new Task ("t002", "Hang Clothes", false);
+		String userId = request.getPathParameters().get("userId");
+		List<Task> tasks = new ArrayList<>();
+		if(userId.equals("abc123")){
+			Task t1 = new Task ("t001", "Iron", false);
+			tasks.add(t1);
+		}else {
+			Task t2 = new Task ("t002", "Hang Clothes", false);
+			tasks.add(t2);
+		}
+
 		//Task t3 = null;
 		//System.out.println(t3.getDescription());
 
-		List<Task> tasks = new ArrayList<>();
-		tasks.add(t1);
-		tasks.add(t2);
-		return ApiGatewayResponse.builder()
+		/*return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(tasks)
-				.build();
+				.build();*/
+
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		response.setStatusCode(200);
+
+		  ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			String responseBody = objectMapper.writeValueAsString(tasks);
+			response.setBody(responseBody);
+		}
+		catch (JsonProcessingException e) {
+			LOG.error("Unable to marshall tasks array", e);
+		}
+		return response;
 	}
 }
