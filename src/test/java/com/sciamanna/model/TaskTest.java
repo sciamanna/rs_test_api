61deb367a43b6e62ef6da4708d194aa876828dae
sciamanna <rebeca.sciamanna@me.com>
2021-02-01 14:36:08 +0000

    Fixed save task

--- src/test/java/com/sciamanna/model/TaskTest.java
+++ src/test/java/com/sciamanna/model/TaskTest.java
@@ -1,15 +1,23 @@
 package com.sciamanna.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
-    @DisplayName("Tesg get task description")
+    @DisplayName("Test get task description")
     @Test
     public void testTaskDescription() {
         Task t = new Task("123", "test task", false);
         assertEquals("test task", t.getDescription(), "Task desc incorrect");
 
+    }
+    @DisplayName("Test complete is set by default")
+    @Test
+    public void testDefaultCompleteStatus() {
+        Task t = new Task();
+        assertFalse(t.isCompleted(),"Task status was not false by defaut");
     }
 }
